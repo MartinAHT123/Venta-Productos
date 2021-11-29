1a55ee8ee5c25d53d5d179a8e5f4f5628279a786
MartinAHT123 <95206863+MartinAHT123@users.noreply.github.com>
2021-11-29 02:20:54 -0500

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -17,5 +17,5 @@ def demo():
 def ventas():
    return render_template("paginas/ventas.html")
 
-if __name__=="__main__":
-   app.run(host='0.0.0.0', port=8000, debug=True)
+#if __name__=="__main__":
+#   app.run(host='0.0.0.0', port=8000, debug=True)
