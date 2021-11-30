7d300a6c5dda92c6194195bfd0837de7b4d9b137
MartinAHT123 <95206863+MartinAHT123@users.noreply.github.com>
2021-11-29 19:01:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 #import pymysql
-#from flask.helpers import url_for
+from flask.helpers import url_for
 from flask import Flask,render_template,request,redirect,flash
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return "Hello World - UNI _FIEE"
-   #return render_template('index.html')
+   #return "Hello World - UNI _FIEE"
+   return render_template('index.html')
 
-#@app.route("/demo")
-#def demo():
-#   return render_template("demo/index.html")
+@app.route("/demo")
+def demo():
+   return render_template("demo/index.html")
 
-#@app.route("/ventas")
-#def ventas():
-#   return render_template("paginas/ventas.html")
+@app.route("/ventas")
+def ventas():
+   return render_template("paginas/ventas.html")
 
 #if __name__=="__main__":
 #   app.run(host='0.0.0.0', port=8000, debug=True)
